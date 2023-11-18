d7cf2fb9465af87157a262e2acf8d134168dbd99
DebugDynasty <zilli7733@gmail.com>
2023-11-18 09:39:19 +0200

    Added logWriter

--- ToDoMVVM/Context.cs
+++ ToDoMVVM/Context.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,24 @@ namespace ToDoMVVM
 {
     public class Context : DbContext
     {
+        private readonly StreamWriter logWriter = new("logs.txt", true);
+
         public DbSet<TaskItem> Tasks { get; set; }
         public Context() => Database.EnsureCreated();
-        //public Context() => Database.EnsureDeleted();
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=tasks.db");
+            optionsBuilder.LogTo(logWriter.WriteLine, LogLevel.Information);
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            logWriter.Dispose();
+        }
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logWriter.DisposeAsync();
         }
     }
 }
